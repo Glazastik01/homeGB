700b226f92718fb86717eb64cab60fd133c18a26
Kirill <vasolovk56@gmail.com>
2022-12-19 16:45:00 +0300

    Добавлены коды для 41 и 43 задачи

---
+++ 41/Program.cs
@@ -0,0 +1,72 @@
+﻿//не понял условие задачи(пользователь должен ввести размер массива или сам массив)
+/*Console.Write("Введите ряд чисел через запятую: ");
+string numsRow = Console.ReadLine() ?? "";
+string[] strArr = numsRow.Split(",");
+int[] intArr = Array.ConvertAll(strArr, int.Parse);
+int k = 0;
+for (int i = 0; i < intArr.Length; i++){
+    if (intArr[i] > 0){
+        k++;
+    }
+}
+Console.WriteLine(k);
+*/
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите корректное число");
+        }
+    }
+    return result;
+}
+
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+int count(int[] array){
+    int k = 0;
+    for (int i = 0; i < array.Length; i++){
+    if (array[i] > 0){
+        k++;
+        }
+    }
+    return k;
+}
+
+int dimension = GetNumber("Введите размерность массива:");
+int[] array = InitArray(dimension);
+PrintArray(array);
+int k = count(array);
+Console.WriteLine($"Количество чисел > 0: {k}");
---
+++ 43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+(double, double) GetX(double k1, double k2, double b1, double b2){
+    double x = (b2-b1)/(k1-k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+(double x1, double y1) = GetX(k1, k2, b1, b2);
+Console.WriteLine($"Точка пересечения будет в месте с координатами {x1}, {y1}");
