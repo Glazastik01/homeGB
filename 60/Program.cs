4038a6b52e9f0b917033dfb051e07163de3ab393
Kirill <vasolovk56@gmail.com>
2022-12-26 17:18:00 +0300

    Добавлены коды для задач 54, 56, 58, 60, 62

---
+++ 60/Program.cs
@@ -0,0 +1,51 @@
+﻿int GetNumber(string message){
+    int result = 0;
+    while(true){
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result)){
+            break;
+        } else {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int rows = GetNumber("Введите высоту:");
+int columns = GetNumber("Введите глубину:");
+int all = GetNumber("Введите ширину:");
+
+int[,,] matrix = Initmatrix3d(rows, columns, all);
+
+int[,,] Initmatrix3d(int rows, int columns, int all){
+    int[,,] originmatr = new int[rows, columns, all];
+    Random rn = new Random();
+    for (int i = 0; i < originmatr.GetLength(0); i++){
+        for (int j = 0; j < originmatr.GetLength(1); j++){
+            for (int k = 0; k < originmatr.GetLength(2); k++){
+                originmatr[i, j, k] = rn.Next(1, 10);
+            }
+        }    
+    }
+    return originmatr;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < matrix.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={matrix[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+
+PrintMatrix(matrix);
---
+++ 62/Program.cs
@@ -0,0 +1,55 @@
+﻿int GetNumber(string message){
+    int result = 0;
+    while(true){
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result)){
+            break;
+        } else {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+
+int rows = GetNumber("Введите высоту:");
+int columns = GetNumber("Введите ширину:");
+
+int[,] matrix = new int[rows, columns];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+if(rows == columns){
+    while (num <= matrix.GetLength(0) * matrix.GetLength(1)){
+        matrix[i, j] = num;
+        num++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1){
+            j++;
+        } else if (i < j && i + j >= matrix.GetLength(0) - 1){
+            i++;
+        } else if (i >= j && i + j > matrix.GetLength(1) - 1){
+            j--;
+        } else {
+        i--;
+        }
+    }
+} else {
+    Console.WriteLine("Невозможно выполнить действие");
+}
+
+PrintMatrix(matrix);
+
+void PrintMatrix (int[,] matrix){
+  for (int i = 0; i < matrix.GetLength(0); i++){
+    for (int j = 0; j < matrix.GetLength(1); j++){
+        if (matrix[i,j] / 10 <= 0){
+            Console.Write($"{matrix[i,j]} ");
+        } else {
+            Console.Write($"{matrix[i,j]} ");
+        }
+    }
+    Console.WriteLine();
+  }
+}
